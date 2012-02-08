e20efc12929dce7564050425b181d7d86a150021
sheimi <sheimi.zhang@gmail.com>
2012-02-08 23:54:46 +0800

    add not

--- src/filter.c
+++ src/filter.c
@@ -9,18 +9,31 @@
 #include <fnmatch.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <time.h>
 
 static Filter filter_tree;
 
 struct stat status;
 FTSENT * cur_ent;
 
+enum time_type {ATIME, AMIN, CTIME, CMIN};
+
 Filter * init_filter();
 void free_filter_node(Filter * node); 
 int exicute_filter(Filter * filter); 
+//filters
+void init_reg(Filter * filter, char * pattern);
 bool reg_filter(Filter * filter);
+
+void init_fnmatch(Filter * filter, char * pattern); 
 bool fnmatch_filter(Filter * filter); 
 
+void init_time(Filter * filter, enum time_type tt, char * pattern);
+bool time_filter(Filter * filter);
+
+void init_not_filter_adapter(Filter * filter);
+bool not_filter_adapter(Filter * filter);
+void set_not_adapter_info (Filter * filter, Filter * inner_filter); 
 
 int exicute_filter(Filter * filter) {
   //test whether this filter_tree is passed
@@ -46,18 +59,29 @@ bool exicute_filter_tree(FTSENT * ent) {
 
 void init_filter_tree(int argc, char * argv[]) {
   int opt;
-  Filter * current_filter = &filter_tree;
+  int not_enable = -1;
+  Filter * now = &filter_tree;
 
   struct option longopts[] = {
-    {"filename", 1, NULL, 'f'},
+    {"not", 0, NULL, 'n'}, 
+    {"name", 1, NULL, 'a'},
     {"regex", 1, NULL, 'r'},
+    {"amin", 1, NULL, 'm'},
+    {"atime", 1, NULL, 't'},
+    {"cmin", 1, NULL, 'i'},
+    {"ctime", 1, NULL, 'e'},
   };
 
-  while((opt = getopt_long(argc, argv, "f:r:", longopts, NULL)) != -1) {
-    current_filter->passed = init_filter();
-    current_filter = current_filter->passed;
+  while((opt = getopt_long(argc, argv, "na:r:m:t:", 
+                           longopts, NULL)) != -1) {
+    Filter * current_filter = init_filter();
     switch(opt) {
-      case 'f':
+      case 'n':
+        fprintf(stderr, "filter not adapter\n");
+        not_enable = 2;
+        init_not_filter_adapter(current_filter);
+        break;
+      case 'a':
         fprintf(stderr, "filename is %s\n", optarg);
         init_fnmatch(current_filter, optarg);
         break;
@@ -65,12 +89,36 @@ void init_filter_tree(int argc, char * argv[]) {
         fprintf(stderr, "regex is %s\n", optarg);
         init_reg(current_filter, optarg);
         break;
+      case 'm':
+        fprintf(stderr, "min deltais %s\n", optarg);
+        init_time(current_filter, AMIN, optarg);
+        break;
+      case 't':
+        fprintf(stderr, "day delta is %s\n", optarg);
+        init_time(current_filter, ATIME, optarg);
+        break;
+      case 'i':
+        fprintf(stderr, "min deltais %s\n", optarg);
+        init_time(current_filter, CMIN, optarg);
+        break;
+      case 'e':
+        fprintf(stderr, "day delta is %s\n", optarg);
+        init_time(current_filter, CTIME, optarg);
+        break;
       case ':':
         fprintf(stderr, "option need a value");
         break;
       case '?':
         fprintf(stderr, "unknown option");
         break;
+    }
+    not_enable--;
+    if (not_enable == 0) {
+      set_not_adapter_info(now, current_filter); 
+      not_enable = false;
+    } else {
+      now->passed = current_filter;
+      now = now->passed;
     }
   }
 }
@@ -90,7 +138,14 @@ Filter * init_filter() {
 void _free_filter(Filter * filter) {
   switch(filter->ft) {
     case REG_FILTER:
-      free_reg(filter);
+      regfree(filter->info);
+      free(filter->info);
+      break;
+    case TIME_FILTER:
+      free(filter->info);
+      break;
+    case NOT_FILTER_ADAPTER:
+      _free_filter(filter->info);
       break;
   }
   free(filter);
@@ -113,6 +168,20 @@ void free_filter_tree() {
   free_filter_node(&filter_tree); 
 }
 
+void init_not_filter_adapter(Filter * filter) {
+  filter->ft = NOT_FILTER_ADAPTER;
+  filter->cmd = not_filter_adapter;
+}
+
+void set_not_adapter_info (Filter * filter, Filter * inner_filter) {
+  filter->info = inner_filter;
+}
+
+bool not_filter_adapter(Filter * filter) {
+  Filter * inner_filter = (Filter *) filter->info;
+  return !inner_filter->cmd(inner_filter);
+}
+
 
 /**
  * regex filter
@@ -125,13 +194,9 @@ void init_reg(Filter * filter, char * pattern) {
   regcomp(filter->info, pattern, cflags);
 }
 
-void free_reg(Filter * filter) {
-  regfree(filter->info);
-}
-
 bool reg_filter(Filter * filter) {
   char * filename = cur_ent->fts_name;
-  char ebuf[128];
+  //char ebuf[128];
   const size_t nmatch = 1;
   regmatch_t pm[1];
   int r;
@@ -158,4 +223,56 @@ bool fnmatch_filter(Filter * filter) {
   if (ret == FNM_NOMATCH)
     return false;
   return true;
+}
+
+/* *
+ * time filter
+ * */
+
+struct time_info {
+  enum time_type tt;
+  char * value; 
+};
+
+void init_time(Filter * filter, enum time_type tt, char * pattern) {
+  struct time_info * ti = malloc(sizeof(struct time_info));
+  ti->tt = tt;
+  ti->value = pattern;
+  filter->info = ti;
+  filter->ft = TIME_FILTER;
+  filter->cmd = time_filter;
+}
+
+bool time_filter(Filter * filter) {
+  struct time_info * ti = filter->info;
+  enum time_type tt = ti->tt;
+  int t_del = atoi(ti->value);
+
+  struct stat buf;
+  time_t cur_time;
+  double diff;
+
+  bool result = false;
+
+  cur_time = time(NULL);
+  stat(cur_ent->fts_path, &buf);
+  switch(tt) {
+    case ATIME:
+      diff = difftime(cur_time, buf.st_atime);
+      result = diff < t_del * 60 * 60 * 24;
+      break;
+    case AMIN:
+      diff = difftime(cur_time, buf.st_atime);
+      result = diff < t_del * 60;
+      break;
+    case CTIME:
+      diff = difftime(cur_time, buf.st_ctime);
+      result = diff < t_del * 60 * 60 * 24;
+      break;
+    case CMIN:
+      diff = difftime(cur_time, buf.st_ctime);
+      result = diff < t_del * 60;
+      break;
+  }
+  return result;
 }
--- src/filter.h
+++ src/filter.h
@@ -9,7 +9,13 @@ struct filter;
 
 typedef bool (*filter_cmd)(struct filter * f);
 
-enum FilterType {FNMATCH_FILTER, REG_FILTER};
+enum FilterType {
+  NOT_FILTER_ADAPTER,
+  FNMATCH_FILTER, 
+  REG_FILTER, 
+  AMIN_FILTER, 
+  TIME_FILTER
+};
 
 typedef struct filter {
   enum FilterType ft;
@@ -23,8 +29,5 @@ typedef struct filter {
 bool exicute_filter_tree(FTSENT * ent);
 void init_filter_tree(int argc, char * argv[]);
 void free_filter_tree();
-void init_reg(Filter * filter, char * pattern);
-void free_reg(Filter * filter);
-void init_fnmatch(Filter * filter, char * pattern); 
 
 #endif
